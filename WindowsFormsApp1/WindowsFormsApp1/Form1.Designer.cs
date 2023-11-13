96443527eee62311cbbf8907507626b0521f609c
StaR <110209766+8Falcon@users.noreply.github.com>
2023-11-13 13:30:45 +0200

    Add files via upload

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtname = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtnumber = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.chkfoot = new System.Windows.Forms.CheckBox();
+            this.chkpasket = new System.Windows.Forms.CheckBox();
+            this.chkswim = new System.Windows.Forms.CheckBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtnumgame = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label6 = new System.Windows.Forms.Label();
+            this.btncalc = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtcostgame = new System.Windows.Forms.TextBox();
+            this.txtvalue = new System.Windows.Forms.TextBox();
+            this.txttotal = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(374, -2);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(241, 46);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "نادى وادى دجلة";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(886, 49);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(150, 37);
+            this.label2.TabIndex = 1;
+            this.label2.Text = ":اسم العضو";
+            // 
+            // txtname
+            // 
+            this.txtname.Location = new System.Drawing.Point(455, 46);
+            this.txtname.Name = "txtname";
+            this.txtname.Size = new System.Drawing.Size(325, 44);
+            this.txtname.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(819, 139);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(238, 37);
+            this.label3.TabIndex = 1;
+            this.label3.Text = ":عدد اعضاء الاسرة";
+            // 
+            // txtnumber
+            // 
+            this.txtnumber.Location = new System.Drawing.Point(680, 132);
+            this.txtnumber.Name = "txtnumber";
+            this.txtnumber.Size = new System.Drawing.Size(100, 44);
+            this.txtnumber.TabIndex = 3;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(936, 211);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(88, 37);
+            this.label4.TabIndex = 1;
+            this.label4.Text = ":اللعبة";
+            // 
+            // chkfoot
+            // 
+            this.chkfoot.AutoSize = true;
+            this.chkfoot.Location = new System.Drawing.Point(652, 207);
+            this.chkfoot.Name = "chkfoot";
+            this.chkfoot.Size = new System.Drawing.Size(128, 41);
+            this.chkfoot.TabIndex = 4;
+            this.chkfoot.Text = "كرة قدم";
+            this.chkfoot.UseVisualStyleBackColor = true;
+            // 
+            // chkpasket
+            // 
+            this.chkpasket.AutoSize = true;
+            this.chkpasket.Location = new System.Drawing.Point(362, 207);
+            this.chkpasket.Name = "chkpasket";
+            this.chkpasket.Size = new System.Drawing.Size(82, 41);
+            this.chkpasket.TabIndex = 4;
+            this.chkpasket.Text = "سلة";
+            this.chkpasket.UseVisualStyleBackColor = true;
+            // 
+            // chkswim
+            // 
+            this.chkswim.AutoSize = true;
+            this.chkswim.Location = new System.Drawing.Point(506, 207);
+            this.chkswim.Name = "chkswim";
+            this.chkswim.Size = new System.Drawing.Size(109, 41);
+            this.chkswim.TabIndex = 4;
+            this.chkswim.Text = "سباحة";
+            this.chkswim.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(645, 295);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(411, 37);
+            this.label5.TabIndex = 1;
+            this.label5.Text = ":عدد الاعضاء المشتركين في اللعبة";
+            // 
+            // txtnumgame
+            // 
+            this.txtnumgame.Location = new System.Drawing.Point(515, 288);
+            this.txtnumgame.Name = "txtnumgame";
+            this.txtnumgame.Size = new System.Drawing.Size(100, 44);
+            this.txtnumgame.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(138, 46);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "احسب";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(244, 430);
+            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(236, 37);
+            this.label6.TabIndex = 1;
+            this.label6.Text = ":قيمة اشتراك اللعبة";
+            // 
+            // btncalc
+            // 
+            this.btncalc.BackColor = System.Drawing.Color.Aqua;
+            this.btncalc.ForeColor = System.Drawing.Color.Black;
+            this.btncalc.Location = new System.Drawing.Point(12, 330);
+            this.btncalc.Name = "btncalc";
+            this.btncalc.Size = new System.Drawing.Size(138, 46);
+            this.btncalc.TabIndex = 6;
+            this.btncalc.Text = "احسب";
+            this.btncalc.UseVisualStyleBackColor = false;
+            this.btncalc.Click += new System.EventHandler(this.btncalc_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(873, 391);
+            this.label7.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(183, 37);
+            this.label7.TabIndex = 1;
+            this.label7.Text = ":قيمة الاشتراك";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(914, 485);
+            this.label8.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(122, 37);
+            this.label8.TabIndex = 1;
+            this.label8.Text = ":الاجمالى";
+            // 
+            // txtcostgame
+            // 
+            this.txtcostgame.Location = new System.Drawing.Point(47, 430);
+            this.txtcostgame.Name = "txtcostgame";
+            this.txtcostgame.Size = new System.Drawing.Size(100, 44);
+            this.txtcostgame.TabIndex = 7;
+            // 
+            // txtvalue
+            // 
+            this.txtvalue.Location = new System.Drawing.Point(669, 384);
+            this.txtvalue.Name = "txtvalue";
+            this.txtvalue.Size = new System.Drawing.Size(138, 44);
+            this.txtvalue.TabIndex = 7;
+            // 
+            // txttotal
+            // 
+            this.txttotal.Location = new System.Drawing.Point(691, 478);
+            this.txttotal.Name = "txttotal";
+            this.txttotal.Size = new System.Drawing.Size(138, 44);
+            this.txttotal.TabIndex = 7;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(20F, 37F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 552);
+            this.Controls.Add(this.txttotal);
+            this.Controls.Add(this.txtvalue);
+            this.Controls.Add(this.txtcostgame);
+            this.Controls.Add(this.btncalc);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.txtnumgame);
+            this.Controls.Add(this.chkswim);
+            this.Controls.Add(this.chkpasket);
+            this.Controls.Add(this.chkfoot);
+            this.Controls.Add(this.txtnumber);
+            this.Controls.Add(this.txtname);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(7, 6, 7, 6);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtname;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtnumber;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox chkfoot;
+        private System.Windows.Forms.CheckBox chkpasket;
+        private System.Windows.Forms.CheckBox chkswim;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtnumgame;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button btncalc;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtcostgame;
+        private System.Windows.Forms.TextBox txtvalue;
+        private System.Windows.Forms.TextBox txttotal;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        int subscription = 10000;
+        short membersubscription = 5000;
+        short football = 500;
+        short swim = 1000;
+        short basketball = 800;
+        int costgame;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btncalc_Click(object sender, EventArgs e)
+        {
+            short number, numbergame;
+            int value;
+
+            number = short.Parse(txtnumber.Text);
+            numbergame = short.Parse(txtnumgame.Text);
+            value = number * membersubscription + subscription;
+
+            if (chkpasket.Checked)
+            {
+                costgame = numbergame * basketball;
+            }
+            else if (chkfoot.Checked)
+            {
+                costgame = numbergame * football;
+            } 
+            else if (chkswim.Checked)
+            {
+                costgame = numbergame * swim;
+            }
+            txtcostgame.Text = costgame.ToString();
+            txtvalue.Text = value.ToString();
+            txttotal.Text = (value + costgame).ToString();
+        }
+    }
+}
